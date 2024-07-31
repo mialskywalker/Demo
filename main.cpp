96b4f4702ccbb82c7b8bf6883ce5bb446f4f3ec1
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-07-31 20:27:45 +0200

    reverse vowels of a string

--- main.cpp
+++ main.cpp
@@ -3,49 +3,30 @@
 
 using namespace std;
 
-bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-    if (flowerbed.size() == 1 && flowerbed[0] == 0) return true;
-    else {
+string reverseVowels(string s) {
+    vector<char> vowels;
+    vector<int> positions;
 
-        for (int i = 0; i < flowerbed.size(); i++) {
-
-            if (flowerbed.size() == 1 && flowerbed[0] == 0)
-                return true;
-            if (i == 0) {
-                if (flowerbed[i] == 0 && flowerbed[i+1] == 0) {
-                    flowerbed.erase(flowerbed.begin());
-                    flowerbed.insert(flowerbed.begin(), 1);
-                    n--;
-                }
-            }
-            else if (i == flowerbed.size()-1) {
-                if (flowerbed[i] == 0 && flowerbed[i-1] == 0) {
-                    flowerbed.erase(flowerbed.end());
-                    flowerbed.push_back(1);
-                    n--;
-                }
-            }
-            else {
-                if (flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
-                    flowerbed.erase(flowerbed.begin() + i);
-                    flowerbed.insert(flowerbed.begin() + i, 1);
-                    n--;
-                }
-            }
-            if (n <= 0) return true;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+            vowels.push_back(s[i]);
+            positions.push_back(i);
         }
-
-        return false;
-
     }
 
+    vector<int>::reverse_iterator it;
+    int k = 0;
+    for (it = positions.rbegin(); it != positions.rend(); it++) {
+        s[*it] = vowels[k];
+        k++;
+    }
+
+    return s;
 }
 
 int main() {
 
-    vector<int> flowerbed {1,0,0,0,1,0,0};
-
-    cout << canPlaceFlowers(flowerbed, 2) << endl;
+    cout << reverseVowels("aA");
 
     return 0;
 }
