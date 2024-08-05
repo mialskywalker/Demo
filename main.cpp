1f13737a0292aab285ab6f24cca51f2173f6e8e8
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-08-05 19:53:06 +0200

    05.08.2024

--- main.cpp
+++ main.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
-bool uniqueOccurrences(vector<int> &arr) {
-    map<int, int> numbers;
-    set<int> unique;
+vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+    vector<vector<int>> res {{},{}};
+    set<int> res1, res2;
 
-    for (int i = 0; i < arr.size(); i++) {
-        if (numbers.find(arr[i]) == numbers.end())
-            numbers[arr[i]] = 1;
-        else
-            numbers[arr[i]]++;
-    }
+    for (vector<int>::iterator it1 = nums1.begin(); it1 != nums1.end(); it1++)
+        if(find(nums2.begin(), nums2.end(), *it1) == nums2.end())
+            res1.insert(*it1);
 
-    for (pair<int, int> el: numbers) {
-        unique.insert(el.second);
-    }
+    for (vector<int>::iterator it2 = nums2.begin(); it2 != nums2.end(); it2++)
+        if(find(nums1.begin(), nums1.end(), *it2) == nums1.end())
+            res2.insert(*it2);
 
-    return numbers.size() == unique.size();
+    for (int el : res1)
+        res[0].push_back(el);
+    for (int el : res2)
+        res[1].push_back(el);
+
+    return res;
 }
 
 int main() {
-    vector<int> arr{-3, 0, 1,-3,1,1,1,-3,10,0};
-    cout << uniqueOccurrences(arr) << endl;
+    vector<int> nums1 {1,2,3,3};
+    vector<int> nums2 {1,1,2,2};
+
+    vector<vector<int>> a = findDifference(nums1, nums2);
+
+    for (int i = 0; i < a.size(); i++) {
+        for (int k = 0; k < a[i].size(); k++) {
+            cout << a[i][k] << ' ';
+        }
+        cout << endl;
+    }
+
 }
