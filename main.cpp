a358e3acb39ee8bf505e35fd6e08429b7eab34e1
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-08-07 19:00:55 +0200

    07.08.2024

--- main.cpp
+++ main.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int findContentChildren(vector<int>& g, vector<int>& s) {
+int removeElement(vector<int>& nums, int val) {
+    int remaining = 0;
 
-    if (s.empty())
-        return 0;
-
-    sort(g.begin(), g.end());
-    sort(s.begin(), s.end());
-
-    int maxNum = 0;
-    int i = 0;
-    int j = 0;
-
-    while (i < g.size() && j < s.size()) {
-        if (s[j] >= g[i]) {
-            maxNum++;
-            i++;
+    for (int i = 0; i < nums.size(); i++)
+        if (nums[i] != val) {
+            nums[remaining] = nums[i];
+            remaining++;
         }
-        j++;
-    }
 
-    return maxNum;
+    return remaining;
 }
 
 int main() {
-    vector<int> g {1,2};
-    vector<int> s {1,2,3};
+    vector<int> nums {0,1,2,2,3,0,4,2};
+    cout << removeElement(nums, 2) << endl;
 
-    cout << findContentChildren(g, s) << endl;
 }
