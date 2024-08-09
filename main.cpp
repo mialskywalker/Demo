9d9a13c074dcc041c0ec9446ca631cf00fd4ed8f
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-08-09 18:45:07 +0200

    09.08.2024

--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int removeElement(vector<int>& nums, int val) {
-    int remaining = 0;
-
-    for (int i = 0; i < nums.size(); i++)
-        if (nums[i] != val) {
-            nums[remaining] = nums[i];
-            remaining++;
-        }
-
-    return remaining;
+string toLowerCase(string s) {
+    for (int i = 0; i < s.length(); i++)
+        s[i] = tolower(s[i]);
+    return s;
 }
 
 int main() {
-    vector<int> nums {0,1,2,2,3,0,4,2};
-    cout << removeElement(nums, 2) << endl;
-
+    cout << toLowerCase("LOVELY") << endl;
 }
